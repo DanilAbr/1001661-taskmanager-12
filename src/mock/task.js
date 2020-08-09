73a1d799f0917c1e412b125521b9d77c4b43051e
Danila <Ltdfm@mail.ru>
2020-08-09 21:27:54 +0300

    Подгоняем моковые данные к реальным

--- src/mock/task.js
+++ src/mock/task.js
@@ -5,9 +5,6 @@ const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-const getRandomElementOfArray = (array) =>
-  array[getRandomInteger(0, array.length - 1)];
-
 const generateDescription = () => {
   const descriptions = [
     `Изучить теорию`,
@@ -15,13 +12,34 @@ const generateDescription = () => {
     `Пройти интенсив на соточку`
   ];
 
-  return getRandomElementOfArray(descriptions);
+  const randomIndex = getRandomInteger(0, descriptions.length - 1);
+
+  return descriptions[randomIndex];
+};
+
+const generateDate = () => {
+  const isDate = Boolean(getRandomInteger(0, 1));
+
+  if (isDate) {
+    return null;
+  }
+
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+  const currentDate = new Date();
+
+  currentDate.setHours(23, 59, 59, 999);
+  currentDate.setDate(currentDate.getDate() + daysGap);
+
+  return new Date(currentDate);
 };
 
 export const generateTask = () => {
+  const dueDate = generateDate();
+
   return {
     descritpion: generateDescription(),
-    dueDate: null,
+    dueDate,
     repeating: {
       mo: false,
       tu: false,
