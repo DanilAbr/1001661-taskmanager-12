bd9b4d3bad3c53ff7dbe1f47acf3bb1c0454acca
Danila <Ltdfm@mail.ru>
2020-08-10 12:26:37 +0300

    Описываем логику отображения дедлайна

--- src/view/task.js
+++ src/view/task.js
@@ -1,3 +1,16 @@
+const isExpired = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  let currentDate = new Date();
+  currentDate.setHours(23, 59, 59, 999);
+  currentDate = new Date(currentDate);
+
+  return currentDate.getTime() > dueDate.getTime();
+};
+
+
 export const createTaskTemplate = (task) => {
   const {color, description, dueDate} = task;
 
@@ -5,7 +18,11 @@ export const createTaskTemplate = (task) => {
     ? dueDate.toLocaleString(`en-US`, {day: `numeric`, month: `long`})
     : ``;
 
-  return `<article class="card card--${color}">
+  const deadlineClassName = isExpired(dueDate)
+    ? `card--deadline`
+    : ``;
+
+  return `<article class="card card--${color} ${deadlineClassName}">
     <div class="card__form">
       <div class="card__inner">
         <div class="card__control">
@@ -19,7 +36,7 @@ export const createTaskTemplate = (task) => {
             type="button"
             class="card__btn card__btn--favorites"
           >
-            favoritesb  
+            favoritesb
           </button>
         </div>
 
