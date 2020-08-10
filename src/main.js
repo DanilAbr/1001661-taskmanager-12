6765e238c1a721ffe20532d1cbdce10d089bb9cd
Danila <Ltdfm@mail.ru>
2020-08-10 09:28:21 +0300

    Используем моки задач

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import {createTaskTemplate} from './view/task.js';
 import {createEditTaskTemplate} from './view/edit-task.js';
 import {createButtonLoadMoreTemplate} from './view/load-button';
 import {render} from './util.js';
+import {generateTask} from "./mock/task.js";
 
 const TASK_COUNT = 3;
+
+const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
@@ -21,7 +24,7 @@ const taskListElement = boardElement.querySelector(`.board__tasks`);
 render(taskListElement, createEditTaskTemplate());
 
 for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate());
+  render(taskListElement, createTaskTemplate(tasks[i]));
 }
 
 render(boardElement, createButtonLoadMoreTemplate());
