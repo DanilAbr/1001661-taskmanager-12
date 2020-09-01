809375d276a57fd465ec30ffbc34bc7bcdb4fa64
Danila <Ltdfm@mail.ru>
2020-09-01 10:02:33 +0300

    Показываем заглушку

--- src/mock/task.js
+++ src/mock/task.js
@@ -67,7 +67,7 @@ export const generateTask = () => {
     dueDate,
     repeating,
     color: getRandomColor(),
-    isArchive: Boolean(getRandomInteger(0, 1)),
+    isArchive: true,
     isFavorite: Boolean(getRandomInteger(0, 1))
   };
 };
