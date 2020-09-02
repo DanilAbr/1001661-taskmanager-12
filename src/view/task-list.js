0d96b584c548bbd3e702d6ccc5051583a9ac4627
Danila <Ltdfm@mail.ru>
2020-09-02 20:47:53 +0300

    НаследувÐет простые компонентыÐ

--- src/view/task-list.js
+++ src/view/task-list.js
@@ -1,23 +1,11 @@
-import {createElement} from "../utils.js";
+import AbstractView from './abstract.js';
 
 const createTaskList = () => {
   return `<div class="board__tasks"></div>`;
 };
 
-export default class TaskList {
-  constructor() {
-    this._element = null;
-  }
-
+export default class TaskList extends AbstractView {
   getTemplate() {
     return createTaskList();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
   }
 }
