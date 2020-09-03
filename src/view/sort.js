181944d4181500312e215411b05e82998f299934
Danila <Ltdfm@mail.ru>
2020-09-03 13:58:20 +0300

    Правим шаблон сортировки

--- src/view/sort.js
+++ src/view/sort.js
@@ -2,9 +2,9 @@ import AbstractView from './abstract.js';
 
 const createSortTemplate = () => {
   return `<div class="board__filter-list">
-            <a href="#" class="board__filter">SORT BY DEFAULT</a>
-            <a href="#" class="board__filter">SORT BY DATE up</a>
-            <a href="#" class="board__filter">SORT BY DATE down</a>
+            <a href="#" class="board__filter" data-sort-type="${SortType.DEFAULT}">SORT BY DEFAULT</a>
+            <a href="#" class="board__filter" data-sort-type="${SortType.DEFAULT}">SORT BY DATE up</a>
+            <a href="#" class="board__filter" data-sort-type="${SortType.DEFAULT}">SORT BY DATE down</a>
           </div>`;
 };
 
