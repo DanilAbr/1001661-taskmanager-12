1053b64f85f2602f81ae6ea9e48200b58a88aa9e
Danila <Ltdfm@mail.ru>
2020-09-03 10:57:37 +0300

    Переносим в презентер метод renderTask

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -5,7 +5,7 @@ import NoTaskView from './view/no-task.js';
 import TaskView from './view/task.js';
 import TaskEditView from './view/task-edit.js';
 import LoadMoreButtonView from './view/load-more-button';
-import {render, RenderPosition} from './utils/render.js';
+import {render, RenderPosition, replace} from './utils/render.js';
 
 const TASK_COUNT_PER_STEP = 8;
 
@@ -32,8 +32,37 @@ export default class Board {
     render(this._boardComponent, this._sortComponent, RenderPosition.AFTERBEGIN);
   }
 
-  _renderTask() {
+  _renderTask(task) {
+    const taskComponent = new TaskView(task);
+    const taskEditComponent = new TaskEditView(task);
 
+    const replaceCardToForm = () => {
+      replace(taskEditComponent, taskComponent);
+    };
+
+    const replaceFormToCard = () => {
+      replace(taskComponent, taskEditComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    taskComponent.setEditClickHandler(() => {
+      replaceCardToForm();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+    taskEditComponent.setFormSubmitHandler(() => {
+      replaceFormToCard();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    render(this._taskListElement, taskComponent, RenderPosition.BEFOREEND);
   }
 
   _renderTasks(from, to) {
