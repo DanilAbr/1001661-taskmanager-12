adb0670041e7f0cc7749a94a165ca75fcbe3ab0f
Danila <Ltdfm@mail.ru>
2020-09-16 22:03:23 +0300

    Добавляет модель в приложение

--- src/main.js
+++ src/main.js
@@ -3,16 +3,20 @@ import FilterView from './view/filter';
 import {generateTask} from './mock/task';
 import {generateFilter} from './mock/filter';
 import BoardPresenter from './presenter/board';
+import TasksModel from './model/tasks';
 import {render, RenderPosition} from './utils/render';
 import {TASK_COUNT} from './const';
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
 
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
+
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(siteMainElement);
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
 render(siteMainElement, new FilterView(filters), RenderPosition.BEFOREEND);
---
+++ src/utils/observer.js
@@ -0,0 +1,17 @@
+﻿export default class Observer {
+  constructor() {
+    this._observers = [];
+  }
+
+  addObserver(observer) {
+    this._Obsorvers.push(observer);
+  }
+
+  removeObserver(observer) {
+    this._observers = this._observers.filter((existedObserver) => existedObserver !== observer);
+  }
+
+  _notify(event, payload) {
+    this._observers.forEach((observer) => observer(event, payload));
+  }
+}
